96992917621a302c56ab2316c5d1762e6aa608b7
kolbymckeown <mckeownkolby@gmail.com>
2020-08-11 21:02:12 -0400

    HomeFeed Styled, TD Render Images

--- client/src/Components/HomeFeed.js
+++ client/src/Components/HomeFeed.js
@@ -1,65 +1,150 @@
 import React from "react";
-import styled from 'styled-components'
-
+import styled from "styled-components";
+import moment from "moment";
+import { BsDot } from "react-icons/bs";
+import { BsChat } from "react-icons/bs";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineUpload } from "react-icons/ai";
 class HomeFeed extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       tweets: null,
-    }
+    };
   }
   componentDidMount() {
     fetch("/api/me/home-feed")
-    .then((res) => {
-      console.log(res);
-      return res.json();
-    })
-    .then((newTweets) => {
-      console.log(newTweets);
-      this.setState({tweets: newTweets});
-    });
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((newTweets) => {
+        console.log(newTweets);
+        this.setState({ tweets: newTweets });
+      });
   }
 
   loop = () => {
     Object.entries(this.state.tweets.tweetsById).map(([tweet, value]) => {
-      console.log(value.status)
-      return (<span>{value.status}</span>) 
-    })
+      console.log(value.status);
+      return <span>{value.status}</span>;
+    });
   };
   render() {
-
     if (this.state.tweets) {
-    var tweetStatuses = Object.entries(this.state.tweets.tweetsById).map(([tweet, value]) => {
-    return (<Li>{value.status}, {value.timestamp}</Li>)
-    // value.numLikes
-    // value.author.avatarSrc
-    // value.author.handle
-    // value.author.displayName
-    // value.author...
-    })
-  } else {
-    return <div>...Loading</div>
+      var tweetStatuses = Object.entries(this.state.tweets.tweetsById).map(
+        ([tweet, value]) => {
+          console.log(value);
+          return (
+            <Li>
+              <div>
+                <Top>
+                  <div>
+                    <AvatarImg src={value.author.avatarSrc} />
+                  </div>
+                  <UserInfo>
+                    <DispName>{value.author.displayName}</DispName>{" "}
+                    <DispHandle>@{value.author.handle}</DispHandle> <BsDot />
+                    <DispTime>
+                      {moment(value.timestamp).format("MMM Do")}
+                    </DispTime>
+                  </UserInfo>
+                </Top>
+                <TweetDetails>
+                  {value.status},{/* <img src={value.media[0].url} /> */}
+                  {/* Get IMG to render?... */}
+                  <TweetFooter>
+                    <BsChat /> <AiOutlineRetweet /> <AiOutlineHeart />{" "}
+                    <AiOutlineUpload />
+                  </TweetFooter>
+                </TweetDetails>
+              </div>
+            </Li>
+          );
+          // value.numLikes
+          // value.author.avatarSrc
+          // value.author.handle
+          // value.author.displayName
+          // value.author...
+        }
+      );
+    } else {
+      return (
+        <div>
+          <Load
+            src="https://giphy.com/embed/WiIuC6fAOoXD2"
+            width="480"
+            height="480"
+            frameBorder="0"
+            class="giphy-embed"
+            allowFullScreen
+          ></Load>
+        </div>
+      );
+    }
+    return (
+      <Wrap>
+        {/* Create Tweet Component goes here? */}
+        <Ul>
+          {this.state.tweets && tweetStatuses}
+          {/* {this.state.tweets && this.state.tweets.tweetsById["1209788222324256768"].status} */}
+        </Ul>
+      </Wrap>
+    );
   }
-  return (
-    <Wrap>
-      
-      <Ul>
-      {this.state.tweets && tweetStatuses}
-      {/* {this.state.tweets && this.state.tweets.tweetsById["1209788222324256768"].status} */}
-      </Ul>
-      
-    </Wrap>
-  );
-};
 }
 
 export default HomeFeed;
 
+const UserInfo = styled.div`
+  color: grey;
+`;
+
+const TweetDetails = styled.div`
+  color: black;
+  margin-top: 10px;
+`;
+
+const TweetFooter = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
+`;
 
+const DispName = styled.span`
+  font-weight: bold;
+  color: black;
+`;
+
+const DispHandle = styled.span`
+  color: grey;
+`;
+
+const DispTime = styled.span`
+  color: grey;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-content: center;
+`;
+
+const AvatarImg = styled.img`
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Load = styled.iframe`
+  position: absolute;
+  top: 20%;
+  left: 45%;
 `;
 
 const Ul = styled.ul`
@@ -67,7 +152,7 @@ const Ul = styled.ul`
 `;
 
 const Li = styled.li`
-  margin: 15px 0px 15px 0px;
+  margin: 15px 0px 50px 0px;
 `;
 
 // GET /api/me/home-feed
